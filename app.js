755ccaa5c11d7d7913970d317eb736817eaa56b2
Mudathir <mudathir3618@gmail.com>
2026-08-18 12:11:13 +0100

    Minor change to see render status

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const fruits = require("./routes/fruits");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Worlds!");
 });
 app.use(express.json());
 
