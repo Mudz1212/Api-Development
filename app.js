ca9d73e4984d7f891a9d07cd7d0d02cf09e185be
Mudathir <mudathir3618@gmail.com>
2026-08-18 12:16:17 +0100

    Added CORS to app.js

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const fruits = require("./routes/fruits");
 
 app.get("/", (req, res) => {
   res.send("Hello Worlds!");
 });
-app.use(express.json());
 
+app.use(cors()); // use applies cors to every http function
+app.use(express.json());
 app.use("/fruits", fruits); // middleware (router) is being used here anything with that endpoint
 
 module.exports = app;
